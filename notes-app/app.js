a2363c4912df8d86500befa0342f4f624357c7a6
antoinequellier <antoinecquellier@gmail.com>
2019-08-14 14:39:44 +0200

    basic arlr with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,45 @@
-const chalk = require ('chalk');
-const getNotes = require ('./notes.js');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const msg = getNotes('Hello');
+// customize yargs version
+yargs.version('1.1.0');
 
-console.log(chalk.green.bold.inverse('Success'));
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    console.log('Adding a new note');
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    console.log('Remove a note');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: () => {
+    console.log('List notes');
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log('Read a note');
+  }
+});
+console.log(yargs.argv)
