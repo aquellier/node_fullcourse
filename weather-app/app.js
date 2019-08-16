5a7ac843cb0d873e766e51ab1b02c9cb388be680
antoinequellier <antoinecquellier@gmail.com>
2019-08-16 16:47:49 +0200

    refacto destructuring and shorthand for weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,21 @@
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-geocode('lille', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+const address = process.argv[2]
+if (address) {
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if(error) {
+      return console.log(error)
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if(error) {
+        return console.log(error)
+      }
+      console.log(location)
+      console.log(forecastData)
+    })
+  })
+} else {
+  console.log('Please provide an address')
+}
 
-forecast('-75.70a88', 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,24 +1,14 @@
 const request = require('request')
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `https://api.darksky.net/forecast/c5881abafc45c4a8d0746468ed904275/${latitude},${longitude}`
-  request({ url: url, json: true }, (error, response) => {
+  const url = `https://api.darksky.net/forecast/c5881abafc45c4a8d0746468ed904275/${latitude},${longitude}?units=si`
+  request({ url, json: true }, (error, { body }) => {
       if (error) {
-          callback('Unable to connect to weather service!')
-      } else if (response.body.error) {
-          callback('Unable to find location')
+          callback('Unable to connect to weather service!', undefined)
+      } else if (body.error) {
+          callback('Unable to find location', undefined)
       } else {
-          callback(`${response.body.daily.data[0].summary}. It is currently ${response.body.currently.temperature} degress out. There is a ${response.body.currently.precipProbability} chance of rain.`)
+          callback(undefined, `${body.daily.data[0].summary}. It is currently ${body.currently.temperature} degress out. There is a ${body.currently.precipProbability} chance of rain.`)
       }
   })
 }
