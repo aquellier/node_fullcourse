9245b8acecd097bd280860afd28f5a3f93277e66
antoinequellier <antoinecquellier@gmail.com>
2019-08-17 17:40:45 +0200

    importing static files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,20 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
-app.get('', (req, res) => {
-  res.send('Hello Express!')
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page!')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About!')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
-  res.send('This is the weather page!')
+  res.send({
+    location: 'Paris',
+    forecast: 27
+  })
 })
 // app.com
 // app.com/help
 // app.com/about
 
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log('Server is up on port 3000')
+})
