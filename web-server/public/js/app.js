713a18a4ecf2da9e00c74fb4cbd79d935025556a
antoinequellier <antoinecquellier@gmail.com>
2019-08-18 13:05:48 +0200

    fetching in js file

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Js gets loaded');
+
+fetch('http://localhost:3000/weather?address=!')
+  .then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.location, data.forecast)
+      }
+    })
+  })
