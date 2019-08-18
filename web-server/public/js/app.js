1d6db800afae680c0cf38d6a595550fd3115338e
antoinequellier <antoinecquellier@gmail.com>
2019-08-18 15:33:02 +0200

    style form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,25 @@
 console.log('Js gets loaded');
 
-fetch('http://localhost:3000/weather?address=!')
-  .then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        console.log(data.error)
-      } else {
-        console.log(data.location, data.forecast)
-      }
-    })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-one')
+const messageTwo = document.getElementById('message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error
+        } else {
+          messageOne.textContent = data.location
+          messageTwo.textContent = data.forecast
+        }
+      })
   })
+})
